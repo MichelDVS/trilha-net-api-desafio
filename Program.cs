c4891872bc5e7638c1602a99c69cd8a48d95c6c2
Michel Douglas <sant.michel28@gmail.com>
2022-11-04 17:43:08 -0300

    Desafio API Tarefa concluído

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using System.Text.Json.Serialization;
 using Microsoft.EntityFrameworkCore;
 using TrilhaApiDesafio.Context;
+using TrilhaApiDesafio.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
